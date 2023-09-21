df44a96d0e36a482145bda74d925f58920868db7
Nerfando-art <102189272+Nerfando-art@users.noreply.github.com>
2023-09-21 11:40:46 -0500

    Add files via upload

---
+++ gradient/script.js
@@ -0,0 +1,38 @@
+const css = document.querySelector("h3");
+const button = document.getElementById("random")
+const color1 = document.querySelector(".color1")
+const color2 = document.querySelector(".color2")
+const body = document.getElementById("gradient")
+
+function changeColor() {
+	body.style.background = 
+	"linear-gradient(to right, "
+	 + color1.value
+	 + ", " 
+	 + color2.value 
+	 + ")"
+
+	 css.textContent = body.style.background + ";";
+}
+
+function randomizeColors() {
+	const randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	const randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+	changeColor();
+}
+
+
+button.addEventListener("click", function() {
+	color1.value = generateRandomColor();
+	color2.value = generateRandomColor();
+	changeColor()
+})
+
+color1.addEventListener("input", changeColor)
+
+color2.addEventListener("input", changeColor)
+
+button.addEventListener("click", randomizeColors)
+
